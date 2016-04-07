2e5128480e67cbc10162bfa18732acf8489bd473
aiguogroup <wangaiguo2546@163.com>
2016-04-07 11:48:56 +0800

    Delete feature_selection_mrmr.m

--- feature_selection_mrmr.m
+++
@@ -1,7 +0,0 @@
-function [final_features] = feature_selection_mrmr(samples, dis_data, labels)
-
-   selected_features = mrmr_mid_d(dis_data, double(labels), 50)'; 
-   
-   final_features = opt_selection(samples, labels, selected_features); 
-   
-end
