740ba422344c79a3b7ddf49d10e9e38e8a0f2258
aiguogroup <wangaiguo2546@163.com>
2016-04-07 11:48:33 +0800

    Delete svm_cv.m

--- svm_cv.m
+++
@@ -1,8 +0,0 @@
-function [cost] = svm_cv(data, label)
-%% Leave One Out Cross Validation
-   nSamples = length(label);
-
-   cmd = ['-v ',num2str(nSamples), ' -t 0',' -c 1'];
-
-   cost = svmtrain(label, data, cmd);
-end
